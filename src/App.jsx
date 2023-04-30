707cf28b4729864f8e3b5553e4b2e9e77c0d272d
Rohan Singh <rohannsingh98765@gmail.com>
2023-04-30 07:05:13 +0530

    routing and home page
    Signed-off-by: Rohan Singh <rohannsingh98765@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { Routes, Route } from "react-router-dom"
+import QuizzPage from "./pages/QuizzPage"
+import ScorePage from "./pages/ScorePage"
+import WelcomePage from "./pages/WelcomePage"
 
 const App = () => {
   return (
-    <div className="font-bold">
-      hello
-    </div>
+    < >
+
+      <Routes>
+        <Route path="/" element={<WelcomePage />}  />
+        <Route path="/quiz" element={<QuizzPage />} />
+        <Route path="/score" element={<ScorePage />} />
+      </Routes>
+
+    </>
   )
 }
 
